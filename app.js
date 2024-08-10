c1a1b33de8b2ba32b38cf786938a74fe0f5bf8d7
hadikrkc <hadi.kirkici@gmail.com>
2024-08-10 16:06:27 +0300

    Added dotenv file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 const routes = require('./routes');
 const sequelize = require('./config/database');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/', routes);
